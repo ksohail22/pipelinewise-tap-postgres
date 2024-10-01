acab25610b8843b61196b077524d2952001d1710
Kashif Sohail <kashif.ezone@gmail.com>
2024-10-01 10:28:53 +0500

    more visibility about the state

--- tap_postgres/sync_strategies/logical_replication.py
+++ tap_postgres/sync_strategies/logical_replication.py
@@ -701,9 +701,11 @@ def sync_tables(conn_info, logical_streams, state, end_lsn, state_file):
                     except Exception:
                         LOGGER.debug('Unable to open and parse %s', state_file)
                     finally:
+                        LOGGER.info('state_comitted\n %s', state_comitted)
                         lsn_comitted = min(
                             get_bookmark(state_comitted, s['tap_stream_id'], 'lsn', start_lsn)  for s in logical_streams
                         )
+                        LOGGER.info(f"start_lsn: {start_lsn}, lsn_currently_processing: {lsn_currently_processing}, lsn_comitted: {lsn_comitted}, lsn_to_flush: {lsn_to_flush}")
                         if (lsn_currently_processing > lsn_comitted) and (lsn_comitted > lsn_to_flush):
                             lsn_to_flush = lsn_comitted
                             LOGGER.info('Confirming write up to %s, flush to %s',
