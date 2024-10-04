ed61920124a110ad173415af9180e50c0b2390bc
ksohail22 <120394453+ksohail22@users.noreply.github.com>
2024-10-04 23:36:15 +0500

    Revert "improved logging"

--- tap_postgres/sync_strategies/logical_replication.py
+++ tap_postgres/sync_strategies/logical_replication.py
@@ -702,9 +702,9 @@ def sync_tables(conn_info, logical_streams, state, end_lsn, state_file):
                         LOGGER.debug('Unable to open and parse %s', state_file)
                     finally:
                         LOGGER.info('state_comitted\n %s', state_comitted)
-                        LOGGER.info('logical_streams\n %s', logical_streams)
-                        lsn_comitted = min([get_bookmark(state_comitted, s['tap_stream_id'], 'lsn') for s in logical_streams])
-
+                        lsn_comitted = min(
+                            get_bookmark(state_comitted, s['tap_stream_id'], 'lsn', start_lsn)  for s in logical_streams
+                        )
                         LOGGER.info(f"start_lsn: {start_lsn}, lsn_currently_processing: {lsn_currently_processing}, lsn_comitted: {lsn_comitted}, lsn_to_flush: {lsn_to_flush}")
                         if (lsn_currently_processing > lsn_comitted) and (lsn_comitted > lsn_to_flush):
                             lsn_to_flush = lsn_comitted
